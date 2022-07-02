22f049087361a8ff9ee7139e94ddc4954d0f1b32
iamjhay <johnnydrille7575@gmail.com>
2022-07-03 00:32:54 +0100

    revised version from R4L by lanre

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,3 +1,36 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+/*===== MENU SHOW ======*/
+// validate if constant exist
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show__menu");
+  });
+}
+
+/*===== MENU HIDDEN ======*/
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show__menu");
+  });
+}
+
+/*=============== REMOVE MOBILE MENU ===============*/
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  //when we click on each nav__link, we remove the show__menu class
+  navMenu.classList.remove("show__menu");
+}
+
+navLink.forEach((n) => {
+  n.addEventListener("click", linkAction);
+});
+
 /*=============== SHOW SCROLL UP ===============*/
 function scrollUp() {
   const scrollup = document.getElementById("scroll-up");
@@ -60,7 +93,7 @@ let mixerSingles = mixitup(".deal__pills--wrapper", {
   },
   animation: {
     duration: 1000,
-    effects: "fade translateY(100%)",
+    effects: "fade",
     reverseOut: true,
     nudge: false,
   },
