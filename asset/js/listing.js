7a7b1fe78bc79c85f75c52112b00f5a7b8dc31c4
iamjhay <johnnydrille7575@gmail.com>
2022-07-09 12:12:07 +0100

    listing page responsive

--- asset/js/listing.js
+++ asset/js/listing.js
@@ -16,6 +16,22 @@ listFull.addEventListener("click", () => {
   listFull.classList.add("active");
 });
 
+/*=============== ACTIVATE 1 X 1 GRID ON SCREEN <=600 @resize & reload ===============*/
+const BrowserResize = () => {
+  if (window.innerWidth <= 600) {
+    listHalf.classList.add("active");
+    listContainer.classList.add("active");
+    listFull.style.display = "none";
+  } else {
+    listFull.style.display = "block";
+    listHalf.classList.remove("active");
+    listFull.classList.add("active");
+    listContainer.classList.remove("active");
+  }
+};
+window.addEventListener("resize", BrowserResize);
+window.addEventListener("DOMContentLoaded", BrowserResize);
+
 let mixerProducts = mixitup(".listing__container", {
   selectors: {
     target: ".cards",
@@ -27,3 +43,74 @@ let mixerProducts = mixitup(".listing__container", {
 
 // Default Filter Products
 mixerProducts.filter("all");
+
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navTheme = document.getElementById("nav__theme"),
+  toast = document.querySelector(".toast"),
+  Message = document.querySelector(".message-text"),
+  progress = document.querySelector(".progress"),
+  navClose = document.getElementById("nav-close");
+
+/*===== MENU SHOW ======*/
+// validate if constant exist
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show__menu");
+  });
+}
+
+/*===== MENU HIDDEN ======*/
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show__menu");
+  });
+}
+
+if (navTheme) {
+  navTheme.addEventListener("click", () => {
+    navMenu.classList.remove("show__menu");
+    console.log(navTheme.childNodes[1].classList.value);
+    if (navTheme.childNodes[1].classList.contains("bx-sun")) {
+      let timer1, timer2;
+      Message.textContent = "Dark Mode Activated";
+      toast.classList.add("active");
+      progress.classList.add("active");
+
+      timer1 = setTimeout(() => {
+        toast.classList.remove("active");
+      }, 5000); //1s = 1000 milliseconds
+
+      timer2 = setTimeout(() => {
+        progress.classList.remove("active");
+      }, 5300);
+    } else {
+      let timer1, timer2;
+      Message.textContent = "Light Mode Activated";
+      toast.classList.add("active");
+      progress.classList.add("active");
+
+      timer1 = setTimeout(() => {
+        toast.classList.remove("active");
+      }, 5000); //1s = 1000 milliseconds
+
+      timer2 = setTimeout(() => {
+        progress.classList.remove("active");
+      }, 5300);
+    }
+  });
+}
+
+/*=============== REMOVE MOBILE MENU ===============*/
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  //when we click on each nav__link, we remove the show__menu class
+  navMenu.classList.remove("show__menu");
+}
+
+navLink.forEach((n) => {
+  n.addEventListener("click", linkAction);
+});
