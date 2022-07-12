a4e06cde0fab861af02c2c03f3115c784dec82e9
iamjhay <johnnydrille7575@gmail.com>
2022-07-12 12:29:13 +0100

    reponsive design

--- asset/js/cart.js
+++ asset/js/cart.js
@@ -13,3 +13,29 @@ const swiperFeatured = new Swiper(".featured__container", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+/*=============== SHOW MODAL ===============*/
+const showModal = (openButton, modalContent) => {
+  const openBtn = document.getElementById(openButton),
+    modalContainer = document.getElementById(modalContent);
+
+  if (openBtn && modalContainer) {
+    openBtn.addEventListener("click", () => {
+      const calendlyWidget = document.querySelector(".calendly-badge-widget");
+      modalContainer.classList.add("show-modal");
+      calendlyWidget.classList.remove("remove-widget");
+    });
+  }
+};
+showModal("open-modal", "modal-container");
+
+/*=============== close MODAL ===============*/
+const closeBtn = document.querySelector(".close-modal");
+
+function closeModal() {
+  const modalContainer = document.getElementById("modal-container");
+  const calendlyWidget = document.querySelector(".calendly-badge-widget");
+  modalContainer.classList.remove("show-modal");
+  calendlyWidget.classList.add("remove-widget");
+}
+closeBtn.addEventListener("click", closeModal);
